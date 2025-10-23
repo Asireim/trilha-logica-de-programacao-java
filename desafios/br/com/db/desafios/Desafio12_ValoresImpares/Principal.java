60d1bfe8353bb4eed5311e584a984368ca4e1dbe
asireim.freitas <asireim.freitas.external@lojasrenner.com.br>
2025-10-23 09:11:09 -0300

    enviando exercicio 12

---
+++ desafios/br/com/db/desafios/Desafio12_ValoresImpares/Principal.java
@@ -0,0 +1,19 @@
+package br.com.db.desafios.Desafio12_ValoresImpares;
+
+public class Principal {
+    public static void main(String[] args) {
+        int[] numeros = {1,2,3,4,5,6,7,8,9,10};
+        int cont = 0;
+
+        System.out.println("Números ímpares:");
+
+        for (int i = 0; i < numeros.length; i++) {
+            if (!(numeros[i] % 2 == 0)) {
+                System.out.println(numeros[i]);
+                cont++;
+            }
+        }
+
+        System.out.println("Quantidade: " + cont);
+    }
+}
